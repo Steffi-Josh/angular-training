475b1622c887a8407a7aa9cae8d5ff492ff845a2
SteffiJosh <steffijoshjosephdevaraj@gmail.com>
2021-11-17 20:39:51 +0530

    commit on master

--- src/app/list-todos/list-todos.component.ts
+++ src/app/list-todos/list-todos.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { TodoDataService } from '../service/data/todo-data.service';
 
 export class Todo {
   constructor(
@@ -6,7 +8,7 @@ export class Todo {
     public description: string,
     public done: boolean,
     public targetDate: Date
-  ){
+  ) {
 
   }
 }
@@ -18,15 +20,47 @@ export class Todo {
 })
 export class ListTodosComponent implements OnInit {
 
-  todos = [
-    new Todo(1,'Onepiece',false,new Date),
-    new Todo(2,'bleach',false,new Date),
-    new Todo(3,'Naruto',false,new Date)
-  ]
+  // [
+  //   new Todo(1,'Onepiece',false,new Date),
+  //   new Todo(2,'bleach',false,new Date),
+  //   new Todo(3,'Naruto',false,new Date)
+  // ]
 
-  constructor() { }
+  todos: Todo[] = [];
+  message !: string;
+
+  constructor(private tododataService: TodoDataService , private route : Router) { }
 
   ngOnInit(): void {
+    this.refreshTodos();
+  }
+
+  updateTodo(id: number) {
+  //  this.tododataService.updateTodo(id);
+    this.route.navigate(['todo',id])
+  }
+
+  addTodo(){
+    this.route.navigate(['todo',-1])
+  }
+
+  refreshTodos() {
+    this.tododataService.retriveAllTodos('admin').subscribe(
+      response => {
+        console.log(response);
+        this.todos = response;
+      }
+    )
+  }
+
+  deleteTodo(id: number) {
+    this.tododataService.deleteTodo('admin', id).subscribe(
+      response => {
+        console.log(response);
+        this.message = `Deletion of Todo ${id} is successfull!`
+        this.refreshTodos();
+      }
+    )
   }
 
 }
